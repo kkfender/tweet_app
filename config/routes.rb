e6da03700a2ffb70dfdd30725436ccd9597e0473
a05kk <kk1kk@protonmail.com>
2018-12-23 05:38:45 +0000

    2nd

--- config/routes.rb
+++ config/routes.rb
@@ -38,11 +38,6 @@ Rails.application.routes.draw do
   
   post "logout" => "users#logout"
   
-  
-  
-  
-   
-   
-  
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
