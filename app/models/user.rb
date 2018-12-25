f43a26b7acc992a9801721de7b6c8874ba7a8f10
a05kk <kk1kk@protonmail.com>
2018-12-25 11:46:36 +0000

    mail validates

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,9 @@ class User < ApplicationRecord
   has_secure_password
   
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
-  validates :name,{presence: true}                   ##存在性
- 
+  validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
+  validates :name,{presence: true}                  #  存在性
+  validates :password, length: { minimum: 8 }       # 「8文字のみ」
  
   
  
